9f03e6aa0591b6906ab638d562399bc08659a84b
weijiamo <weijiamo@hotmail.com>
2016-04-24 00:22:41 +0800

    update

--- src/components/StockView.vue
+++ src/components/StockView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bs-docs-section" id="tabs">
     <div v-gesture:swipe="test($event, 'tst')" class="bs-example">
-      <tabs>
+      <tabs v-ref:myTabs>
         <tab header="hello one">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -13,43 +13,36 @@
           </p>
         </tab>
         <tab header="hello two">
-           <p>
-              Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.
-           </p>
+           <kline></kline>
         </tab>
-        <tab header="three" disabled>
-          ...
+        <tab header="three">
+          hello world!
         </tab>
       </tabs>
     </div>
-    <pre><code class="language-markup"><script type="language-mark-up">
-<tabs :active="0">
-  <tab header="one">
-    ...
-  </tab>
-  <tab header="two">
-    ...
-  </tab>
-  <tab header="three" disabled>
-    ...
-  </tab>
-</tabs>
-</script></code></pre>
 </template>
 
 <script>
   import tabs from 'vue-strap/src/Tabset.vue'
   import tab from 'vue-strap/src/Tab.vue'
+  import kline from 'src/components/kLine.vue'
+  
   export default {
     components: {
       tabs,
-      tab
+      tab,
+      kline
     },
     methods: {
       test: function(event, msg) {
-        console.log(tabs);
-        //tabs.activeIndex = 2;
-        console.log(msg );
+        var tabs2 = Vue.component('tabs')
+        console.log(this.$refs.mytabs);
+        var index = this.$refs.mytabs.activeIndex + 1;
+        if (this.$refs.mytabs.$children.length <= index) {
+          index = 0;
+        }
+        this.$refs.mytabs.activeIndex = index;
+        console.log(index);    
       }
     }
   }
---
+++ src/components/kLine.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+</template>
+
+<script>
+  export default {
+  components: {
+    },
+      ready: function(event, msg) {
+        var dataPoints = [];
+    var y = 0;
+    var limit = 50000;
+    
+    for ( var i = 0; i < limit; i++ ) {
+      
+      y += Math.round( 10 + Math.random() * (-10 -10)); 
+      dataPoints.push({ y: y});
+    }
+    
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      animationEnabled: true,
+      zoomEnabled: true,
+      zoomType: "xy",
+      
+      title:{
+        text: "Performance Demo with 50,000 DataPoints"
+      },    
+      data: [
+      {
+        type: "line",              
+        dataPoints: dataPoints
+      }
+      ]
+    });
+    chart.render();
+      }
+  }
+</script>
